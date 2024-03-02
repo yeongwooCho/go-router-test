87c712d7f98590b973d79bec36161031bc403ea7
yeongwoo <jyy0223@naver.com>
2024-03-02 22:57:07 +0900

    [add] GoRoute 를 이용해 화면을 트리구조로 구성 및 BasicScreen 으로 Go 기능 추가

--- lib/route/route.dart
+++ lib/route/route.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_theory/screens/1_basic_screen.dart';
 import 'package:go_router_theory/screens/root_screen.dart';
 
 final router = GoRouter(
@@ -8,6 +9,14 @@ final router = GoRouter(
       builder: (context, state) {
         return RootScreen();
       },
+      routes: [
+        GoRoute(
+          path: 'basic',
+          builder: (context, state) {
+            return BasicScreen();
+          },
+        ),
+      ]
     ),
   ],
 );
